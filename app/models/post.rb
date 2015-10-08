d7cc7de5c36e1e8e916e786575a18548e297196c
Lauren Burris <lmburris@gmail.com>
2015-10-07 20:45:56 -0700

    updated blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+	validates :slug, uniqueness: true, presence: true
+	before_validation :generate_slug
+	has_many :comments
+	def to_param
+		slug
+	end
+	
+	def generate_slug
+		self.slug ||= title.parameterize
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008020941) do
+ActiveRecord::Schema.define(version: 20151008032856) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20151008020941) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug"
 
 end
